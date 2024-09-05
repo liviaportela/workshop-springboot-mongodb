f5ff16bb65efd253ba5c0ed7dcda935a330f231a
Lívia <liviaportelaferreira@gmail.com>
2024-09-05 16:47:32 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/liviaportela/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.liviaportela.workshopmongo.repository;
+
+import com.liviaportela.workshopmongo.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User,String> {
+
+}
---
+++ src/main/java/com/liviaportela/workshopmongo/services/UserService.java
@@ -0,0 +1,19 @@
+package com.liviaportela.workshopmongo.services;
+
+import com.liviaportela.workshopmongo.domain.User;
+import com.liviaportela.workshopmongo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repo;
+
+    public List<User> findAll(){
+        return repo.findAll();
+    }
+}
