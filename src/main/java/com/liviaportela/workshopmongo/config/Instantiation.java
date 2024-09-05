32f2989763d11dd7c7f5cc5310217cf7c1825df1
Lívia <liviaportelaferreira@gmail.com>
2024-09-05 20:47:22 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/liviaportela/workshopmongo/config/Instantiation.java
+++ src/main/java/com/liviaportela/workshopmongo/config/Instantiation.java
@@ -2,6 +2,7 @@ package com.liviaportela.workshopmongo.config;
 
 import com.liviaportela.workshopmongo.domain.Post;
 import com.liviaportela.workshopmongo.domain.User;
+import com.liviaportela.workshopmongo.dto.AuthorDTO;
 import com.liviaportela.workshopmongo.repository.PostRepository;
 import com.liviaportela.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,10 +35,11 @@ public class Instantiation implements CommandLineRunner {
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
-        Post post1 = new Post(null, sdf.parse("11/12/2021"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
-
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+        Post post1 = new Post(null, sdf.parse("11/12/2021"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+
         postRepository.saveAll(Arrays.asList(post1, post2));
     }
 }
