7b2cbdbd189c4672823e1ba51cf040ffb1895f3c
Lívia <liviaportelaferreira@gmail.com>
2024-09-05 21:06:57 -0300

    Obtendo um post por id

---
+++ src/main/java/com/liviaportela/workshopmongo/resources/PostResource.java
@@ -0,0 +1,26 @@
+package com.liviaportela.workshopmongo.resources;
+
+import com.liviaportela.workshopmongo.domain.Post;
+import com.liviaportela.workshopmongo.services.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource {
+
+    @Autowired
+    private PostService service;
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET) //@GetMapping (outra opção)
+    public ResponseEntity<Post> findById(@PathVariable String id){
+        Post obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+}
