0b3e2aa83bf0d4fe7ff31cd3ae33c3a441c9194c
Lívia <liviaportelaferreira@gmail.com>
2024-09-05 21:45:32 -0300

    Consulta simples com query methods

--- src/main/java/com/liviaportela/workshopmongo/resources/PostResource.java
+++ src/main/java/com/liviaportela/workshopmongo/resources/PostResource.java
@@ -1,15 +1,13 @@
 package com.liviaportela.workshopmongo.resources;
 
 import com.liviaportela.workshopmongo.domain.Post;
+import com.liviaportela.workshopmongo.resources.util.URL;
 import com.liviaportela.workshopmongo.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.net.URI;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -22,5 +20,12 @@ public class PostResource {
     public ResponseEntity<Post> findById(@PathVariable String id){
         Post obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @RequestMapping(value="/titlesearch", method=RequestMethod.GET)
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text) {
+        text = URL.decodeParam(text);
+        List<Post> list = service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
     }
 }
