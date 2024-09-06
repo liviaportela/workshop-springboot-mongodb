4dc5a9fd15d887c66f0a221b4c9b9b88bcc2ec04
Lívia <liviaportelaferreira@gmail.com>
2024-09-05 21:00:16 -0300

    Endpoint para retornar os posts de um usuário

--- src/main/java/com/liviaportela/workshopmongo/resources/UserResource.java
+++ src/main/java/com/liviaportela/workshopmongo/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.liviaportela.workshopmongo.resources;
 
+import com.liviaportela.workshopmongo.domain.Post;
 import com.liviaportela.workshopmongo.domain.User;
 import com.liviaportela.workshopmongo.dto.UserDTO;
 import com.liviaportela.workshopmongo.services.UserService;
@@ -52,5 +53,11 @@ public class UserResource {
         obj.setId(id);
         obj = service.update(obj);
         return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(value = "/{id}/posts", method = RequestMethod.GET) //@GetMapping (outra opção)
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj.getPosts());
     }
 }
